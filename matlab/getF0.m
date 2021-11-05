68b5a4758cc2465a6a5e14c410dd64a2a1a43429
Giang Le <gianghl2@illinois.edu>
2021-11-05 17:38:10 -0500

    matlab

--- matlab/getF0.m
+++ matlab/getF0.m
@@ -1,4 +1,4 @@
-myFolder = '/Users/gianghale/Desktop/ProsodyPro/m-3-q/channel1';
+myFolder = '\\client\h$\Desktop\ProsodyPro\m-3-78\channel1';
 myFiles = dir(fullfile(myFolder,'*.wav')); %gets all wav files
 
 for k = 1:length(myFiles)
@@ -7,9 +7,12 @@ for k = 1:length(myFiles)
   fprintf(1, 'Now reading %s\n', fullFileName);
   [y, Fs] = audioread(fullFileName);
   [f0, ~] = pitchRocco(y, Fs);
+  i = 1:length(f0);
+  i_new =  linspace(min(i), max(i), 20);
+  f0_downsampled = interp1(i, f0, i_new, 'linear');  
   fid= fopen(fullfile(myFolder, 'F0.mat'),'a');
   fprintf(fid, '%s ', baseFileName);
-  fprintf(fid, '%f ', f0);
+  fprintf(fid, '%f ', f0_downsampled);
   fprintf(fid,'\n');
   fclose(fid);
 end
